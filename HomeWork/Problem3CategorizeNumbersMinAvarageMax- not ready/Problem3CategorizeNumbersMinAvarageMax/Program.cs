f5723ac64d2ea115bec9882d1408dc1818c18d3c
AlexDimit <sa6onick@abv.com>
2015-09-29 14:27:45 +0300

    homework
    none

---
+++ HomeWork/Problem3CategorizeNumbersMinAvarageMax- not ready/Problem3CategorizeNumbersMinAvarageMax/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem3CategorizeNumbersMinAvarageMax
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            float[] num  = new float[input.Length];
+            float[] Rounded = new float[input.Length];
+            float[] Floating = new float[input.Length];
+            int FlagForCycle = 0;
+            float temp = 0;
+            int Roundedmin = 0;
+            float Floatingmin = 0f;
+            float RoundedSum = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                num[i] = float.Parse(input[i]);
+                if ((num[i] % 1) > 0)
+                {
+                    Floating[i] = num[i];
+                }
+                else Rounded[i] = num[i];
+            }        
+           
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (Rounded[i] != 0)
+                {
+                    RoundedSum +=  Rounded[i];
+                    FlagForCycle++;
+                    Roundedmin = (int) Rounded[i];
+                    if (i<input.Length-1 &&(Rounded[i] < Rounded[i + 1]))
+                    {
+                        temp = Rounded[i];
+                        if (temp < Roundedmin)
+                            Roundedmin = (int)temp;
+                        else break;
+                    }
+                    Console.Write(Rounded[i] + " ");
+                  
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine(Roundedmin + ":min! ");
+            Console.WriteLine(Rounded.Max() + ":max! ");
+            Console.WriteLine(RoundedSum + ":sum! ");
+            Console.WriteLine(RoundedSum/FlagForCycle + ":avarage! ");
+            Console.WriteLine();
+
+            RoundedSum=0;
+            FlagForCycle = 0;
+            temp = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (Floating[i] != 0)
+                {
+                    RoundedSum += Floating[i];
+                    FlagForCycle++;
+                    Floatingmin = Floating[i];
+                    if (i < input.Length - 1 && (Floating[i] < Floating[i + 1]))
+                    {
+                        temp = Floating[i];
+                        if (temp < Floatingmin)
+                        {
+                            Floatingmin = temp;
+                        }
+                        else break;
+                        
+                    }
+
+                }
+                if (Floating[i] != 0)
+                Console.Write(Floating[i] + " ");
+
+            }
+            Console.WriteLine();
+            Console.WriteLine(Floatingmin + ":min! ");
+            Console.WriteLine(Floating.Max() + ":max! ");
+            Console.WriteLine(RoundedSum + ":sum! ");
+            Console.WriteLine(RoundedSum / FlagForCycle + ":avarage! ");
+            Console.WriteLine();
+            
+        }
+    }
+}
---
+++ HomeWork/Problem8LegoBlocks/Problem8LegoBlocks/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem8LegoBlocks
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Rows!:");
+            int rows = int.Parse(Console.ReadLine());
+
+            List<List<int>> firstList = new List<List<int>>();
+            List<List<int>> secondList = new List<List<int>>();
+            bool Rectanguler = false;
+            int count = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                Console.Write("Add elements in the first List!:");
+               firstList.Add(new List<int>());
+               int[] Array = Console.ReadLine().Trim().Split().Select(int.Parse).ToArray();
+               for (int j = 0; j < Array.Length; j++)
+               {
+                   firstList[i].Add(Array[j]);
+                  
+               }
+            }
+            foreach(List<int> el in firstList)
+            {
+                count += el.Count();
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                Console.Write("Add elements in the Second List!:");
+                secondList.Add(new List<int>());
+                int[] Array = Console.ReadLine().Trim().Split().Select(int.Parse).ToArray();
+                for (int j = 0; j < Array.Length; j++)
+                {
+                    secondList[i].Add(Array[j]);
+
+                }               
+            }
+
+            foreach (List<int> el in secondList)
+            {
+                count += el.Count();
+            }
+
+            for (int i = 0; i < firstList.Count(); i++)
+            {
+                secondList[i].Reverse();
+               firstList[i].AddRange(secondList[i]);
+            }
+
+           int rowLength = firstList[0].Count();
+           for (int i = 1; i < firstList.Count; i++)
+           {
+               if (rowLength == firstList[i].Count())
+               {
+                   Rectanguler = true;
+               }
+               else
+               {
+                   Rectanguler = false;
+                   break;
+               }
+           }
+           if (Rectanguler)
+           {
+               for (int k = 0; k < rows; k++)
+               {
+                   Console.Write("[");
+                   for (int j = 0; j < firstList[k].Count; j++)
+                   {
+                       Console.Write("{0} ", firstList[k][j]);
+                   }
+                   Console.Write("]");
+                   Console.WriteLine();
+               }
+           }
+           else Console.WriteLine(count + "total count of cells is:");
+
+            
+
+        }
+    }
+}
