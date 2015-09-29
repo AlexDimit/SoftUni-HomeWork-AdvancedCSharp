be4a90771cf2b482d543120461b06af6fe6ab483
AlexDimit <sa6onick@abv.com>
2015-09-29 14:37:09 +0300

    HomeWork

---
+++ HomeWork/Problem4SequenceinMatrix - 95%/Problem4SequenceinMatrix/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem4SequenceinMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("rows:");
+            int rows = int.Parse(Console.ReadLine());
+            Console.Write("Collumns:");
+            int colls = int.Parse(Console.ReadLine());
+            int flag = 1;
+            int flagForColl = 1;
+            int flagForDiag = 1;         
+            List<string> listRow = new List<string>();
+            List<string> listCol = new List<string>();
+            List<string> listDiag = new List<string>();
+
+            string[,] matrix = new string[rows, colls];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < colls; j++)
+                {
+                    string inputNumber = Console.ReadLine();
+                    matrix[i, j] = inputNumber;
+                }
+            }
+
+
+            for (int i = 0; i < rows; i++) // for rows!
+            {
+                for (int j = 0; j < colls-1; j++)
+                {
+                    if (matrix[i, j].Equals(matrix[i, j + 1]))
+                    {
+                        flag++;
+                        listRow.Add(matrix[i, j]);
+                        listRow.Add(matrix[i, j + 1]);                      
+                    }
+                }
+            }
+
+            for (int i = 0; i < rows-1; i++) // for collumns!
+            {
+                for (int j = 0; j < colls ; j++)
+                {
+                    if (matrix[i, j].Equals(matrix[i + 1, j]))
+                    {
+                        flagForColl++;
+                        listCol.Add(matrix[i, j]);
+                        listCol.Add(matrix[i + 1, j]);
+                    }                   
+                }
+            }
+            for (int i = 0; i < rows - 1; i++) // for diagonals!
+            {
+                for (int j = 0; j < colls-1; j++)
+                {
+                    if (matrix[i, j].Equals(matrix[i + 1, j+1]))
+                    {
+                        flagForDiag++;
+                        listDiag.Add(matrix[i, j]);
+                        listDiag.Add(matrix[i + 1, j+1]);
+                    }
+                }
+            }
+
+
+ 
+            //for (int i = 0; i < rows; i++)
+            //{
+            //    for (int j = 0; j < colls; j++)
+            //    {
+            //        Console.Write(matrix[i, j] + " ");
+            //    }
+            //     Console.WriteLine();
+            //}                        
+            Console.WriteLine();
+            if(flag > flagForColl && flag > flagForDiag)
+            for (int i = 0; i < flag; i++)
+            {
+                Console.Write(listRow[i] + " ");
+            }
+            
+            if (flagForColl > flag && flagForColl > flagForDiag)
+            for (int i = 0; i <= flagForColl; i++)
+            {
+                if (listCol[i] == listCol[i + 1])
+                {
+                    int j = 0;
+                    j++;
+                    Console.Write(listCol[j] + " ");                   
+                }
+            }
+
+            if(flagForDiag > flag && flagForDiag > flagForColl)
+            for (int i = 0; i < flagForDiag; i++)
+            {
+                Console.Write(listDiag[i] + " ");
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ HomeWork/Problem8NightLife/Problem8NightLife/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem8NightLife
+{
+    class Program
+    {                   
+        public static Dictionary<string, SortedDictionary<string, SortedSet<string>>> citiesNightLife = new Dictionary<string, SortedDictionary<string, SortedSet<string>>>();
+        static void Main(string[] args)
+        {
+           
+            while(true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "END")
+                    break;
+
+                string[] nightLifeInfo = getInputInfo(input);
+                
+                if(!citiesNightLife.ContainsKey(nightLifeInfo[0]))
+                {
+                    SortedDictionary<string, SortedSet<string>> newCity = new SortedDictionary<string,SortedSet<string>>();
+
+                    citiesNightLife.Add(nightLifeInfo[0], newCity);
+                }
+                
+                if (!citiesNightLife[nightLifeInfo[0]].ContainsKey(nightLifeInfo[1]))
+                {
+                    citiesNightLife[nightLifeInfo[0]].Add(nightLifeInfo[1], new SortedSet<string>());
+                }
+
+                citiesNightLife[nightLifeInfo[0]][nightLifeInfo[1]].Add(nightLifeInfo[2]);
+
+            }
+
+            printNightLifeInfo();
+
+            Console.ReadLine();
+        }
+
+        static void printNightLifeInfo()
+        {
+            foreach(string key in citiesNightLife.Keys)
+            {
+                Console.WriteLine(key);
+                var venueKeys = citiesNightLife[key].Keys;
+
+                foreach (var venue in venueKeys)
+                {
+                    Console.WriteLine("->{0} : {1}", venue, string.Join(", ", citiesNightLife[key][venue]));
+                }
+            }
+        }
+
+        static string[] getInputInfo(string input)
+        {
+            string[] result = new string[3];
+            int firstSemiColon = input.IndexOf(';');
+            int lastSemiColon = input.LastIndexOf(';');
+            StringBuilder sb = new StringBuilder(input.Remove(lastSemiColon));
+
+            sb.Remove(0, firstSemiColon + 1);
+
+            result[0] = input.Remove(firstSemiColon);
+            result[1] = sb.ToString();
+            result[2] = input.Remove(0, lastSemiColon + 1);
+            
+            return result;
+        }
+
+        }
+    }
